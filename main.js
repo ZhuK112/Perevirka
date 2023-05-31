d4984f00a8573a3e798a22a8729b933ae449aaa4
ZhuK <127308032+ZhuK112@users.noreply.github.com>
2023-05-31 17:28:37 +0300

    123

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-function showPopup() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-  
-    document.getElementById('popup-name').textContent = name;
-    document.getElementById('popup-email').textContent = email;
-  
-    document.getElementById('popup').style.display = 'block';
-  }
+console.log(jQuery().jquery);
+//цей скрипт для меню «бургера»
+$(document).on('click', '#burger', function () {
+ $('.nav-menu').toggleClass('active');
+});
+// Submit subscription form using Ajax
+$('#subscription_form').on('submit', function (e) {
+ e.preventDefault();
+ var $form = $(this);
+ $.ajax({
+ type: 'POST',
+ url: 'subscription_ajax.php',
+ data: $form.serialize()
+ }).done(function () {
+ $form[0].reset()
+ alert('Thank you for the subscription!')
+ }).fail(function () {
+ alert('Something went wrong')
+ });
+});
